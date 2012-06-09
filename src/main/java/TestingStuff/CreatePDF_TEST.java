8a3d85b027bd897c72aa60084522567768f1d42f
Angello Pozo <sogoiii@gmail.com>
2012-06-08 19:05:16 -0700

    Can create multiple paged pdf files with correct qrcodes (SEE NOTE)
    -Downgraded to pdfbox 1.6 because for some reason 1.7 only creates a single QRcode image for each page. By simply downgrading, no code changes, each question correctly has its own QR code
    
    Signed-off-by: Angello Pozo <sogoiii@gmail.com>

--- src/main/java/TestingStuff/CreatePDF_TEST.java
+++ src/main/java/TestingStuff/CreatePDF_TEST.java
@@ -1,6 +1,7 @@
 package TestingStuff;
 
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
@@ -11,12 +12,16 @@ import java.util.Hashtable;
 import java.util.List;
 
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import org.apache.pdfbox.exceptions.COSVisitorException;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDTrueTypeFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.pdmodel.graphics.xobject.PDJpeg;
 import org.apache.pdfbox.pdmodel.graphics.xobject.PDXObjectImage;
@@ -38,7 +43,7 @@ import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 public class CreatePDF_TEST {
 	
-	static int sizeofQRCode = 100; //this is teh size of the QR code 
+	static int sizeofQRCode = 100; //this is the size of the QR code 
 
 	
 	String testrepo = "/Users/angellopozo/Dropbox/My Code/java/MainRabbitMongo/Resources/";
@@ -57,7 +62,7 @@ public class CreatePDF_TEST {
 		Question Q2 = new Question();
 		Q2.setQuestion("Who was the 16th President of the United States?");
 		Q2.setAnswer("Abraham Lincoln");
-		String[] Q2PA = {"Anderw Johnson","Franklin Pierce", "John Tayler"};
+		String[] Q2PA = {"Anderw Johnson","Franklin Pierce", "John Tayler", "whowhatnow?"};
 		Q2.setPossibleAnswer(Q2PA);
 		
 		Question Q3 = new Question();
@@ -69,7 +74,7 @@ public class CreatePDF_TEST {
 		Question Q4 = new Question();
 		Q4.setQuestion("you can convert it to a BufferedImage by copying its raster to a new BufferedImage. Serach for 'convert image to bufferedimage' ");
 		Q4.setAnswer("Abraham Lincoln");
-		String[] Q4PA = {"Anderw Johnson","Franklin Pierce", "John Tayler"};
+		String[] Q4PA = {"Anderw Johnson","Music video by Coldplay performing Paradise. (C) 2011 EMI Records Ltd This label copy information is the subject of copyright protection.", "John Tayler"};
 		Q4.setPossibleAnswer(Q4PA);
 		
 		Question Q5 = new Question();
@@ -91,7 +96,26 @@ public class CreatePDF_TEST {
 		String[] Q7PA = {"Anderw Johnson","Franklin Pierce", "John Tayler"};
 		Q7.setPossibleAnswer(Q7PA);
 		
-		Question[] QA = new Question[7];		
+		Question Q8 = new Question();
+		Q8.setQuestion("Who was the 16th President of the United States?");
+		Q8.setAnswer("Abraham Lincoln");
+		String[] Q8PA = {"Anderw Johnson","Franklin Pierce", "John Tayler"};
+		Q8.setPossibleAnswer(Q8PA);
+		
+		Question Q9 = new Question();
+		Q9.setQuestion("Who was the 16th President of the United States?");
+		Q9.setAnswer("Abraham Lincoln");
+		String[] Q9PA = {"Anderw Johnson","Franklin Pierce", "John Tayler"};
+		Q9.setPossibleAnswer(Q9PA);
+		
+		Question Q10 = new Question();
+		Q10.setQuestion("Who was the 16th President of the United States?");
+		Q10.setAnswer("Abraham Lincoln");
+		String[] Q10PA = {"Anderw Johnson","Franklin Pierce", "John Tayler"};
+		Q10.setPossibleAnswer(Q10PA);
+		
+		
+		Question[] QA = new Question[10];		
 		QA[0] = Q1;
 		QA[1] = Q2;
 		QA[2] = Q3;
@@ -99,79 +123,117 @@ public class CreatePDF_TEST {
 		QA[4] = Q5;
 		QA[5] = Q6;
 		QA[6] = Q7;
+		QA[7] = Q8;
+		QA[8] = Q9;
+		QA[9] = Q10;
 		
 		numofquestions = QA.length;
 		System.out.println("num of q = " + numofquestions);
+		PDFont Questionfont = PDType1Font.HELVETICA; //set font of pdf
 		
 		 BufferedImage bubblgeImage = ImageIO.read(new File("/Users/angellopozo/Dropbox/My Code/java/MainRabbitMongo/Resources/LargeBubble2.jpg"));
 		
 //		 PDDocument doc = PDDocument.load(new File("/Users/angellopozo/Documents/workspace/CreatePDF_TestV1/mycreation2.pdf"));
 		 PDDocument doc = new PDDocument();
-		 PDPage blankPage = new PDPage(); //intialize blank page
-		 doc.addPage( blankPage ); //add blank page to doc
+//		 doc.addPage( new PDPage() ); //add blank page to doc
 		 
-	     PDPage page = (PDPage)doc.getDocumentCatalog().getAllPages().get(0);//get first page //at least start here!
+//	     PDPage page = (PDPage)doc.getDocumentCatalog().getAllPages().get(0);//get first page //at least start here!
 	   
+		 
+		 
+		 
+		 
+		 JFrame frame = new JFrame(); //window popup 
+	     int remaining = numofquestions; //number of questions written = remaining
+	     int pageindex = 0; //this is the number of pages
+	     int Questionshift = 0; //
+	     while(remaining > 0){
+	     
+		     doc.addPage( new PDPage() ); //add blank page to doc
+		     System.out.println("pageindex  = " + pageindex);
+		     PDPage currentpage = (PDPage)doc.getDocumentCatalog().getAllPages().get(pageindex);//get current page
+		     WriteTitleToPDF( doc, currentpage); //write test title on top of page
+		     
+		     System.out.println("Created page " + pageindex);
+		     System.out.println("remaining =  " + remaining);
+		     int i = 0;
+		    //CREATE THE PDF MAIN LOOP
+				while(i < 7){
+					String qrCodeText = "Question "+i;
+			        BufferedImage aQRImage = null; //initialize new bufferedimage, this will be the qrcode
+			        aQRImage = createQRImage(qrCodeText, sizeofQRCode, "png"); //create buffered image of QRCode
+
+			        PDXObjectImage PDQRImage = new PDJpeg(doc, aQRImage); //make image object for PDFbox
+			        BufferedImage testtest = PDQRImage.getRGBImage();
+			        
+				       frame.getContentPane().setLayout(new FlowLayout());
+				       frame.getContentPane().add(new JLabel(new ImageIcon(testtest)));
+				       frame.pack();
+				       frame.setVisible(true);
+			        
+			        
+			        PDXObjectImage bubble = new PDJpeg(doc, bubblgeImage); //make image object for PDFbox
+			        PDPageContentStream contentStream = new PDPageContentStream(doc, currentpage,true,false); //create write stream
+			        contentStream.drawImage( PDQRImage,0, (650-105*i) ); //write Qrcode onto pdf
+			        contentStream.setFont( Questionfont, 10 );//set font
+			        WriteQuestionToPDF(contentStream, i, QA,bubble);//write the Question, answers, and bubbles
+			        
+			        System.out.println("DONE writing question num = "+i);
+			        contentStream.close();//close stream 
+			        remaining--;
+			        i++;
+			        if(remaining == 0){
+			        	break;
+			        }
+				}//end of second while loop     
+			pageindex++; //increment next page
+			Questionshift = Questionshift + 7;
+			System.out.println("questionshift = " + Questionshift); 
+			
+	     }//end of fir while loop for pages
+
+		doc.save( "/Users/angellopozo/Dropbox/My Code/java/MainRabbitMongo/Resources/CreatedPDF_TEST_Franklin_Gothic_Book_CTV1_16.pdf");
+		doc.close();
+		
+	
+	}//end of main
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	private static void WriteTitleToPDF(PDDocument doc, PDPage page) throws IOException{
+		
+		//chose font
+//	     PDFont font = PDTrueTypeFont.loadTTF( doc, new File( "/Library/Fonts/Khmer Sangam MN.ttf" ) );
+//	     PDFont font = PDTrueTypeFont.loadTTF( doc, new File( "/Library/Fonts/Microsoft/Franklin Gothic Book.ttf" ) );
 	     PDFont font = PDType1Font.HELVETICA_BOLD; //set font of pdf
-	     
-	     
+		
 	     //CREATE TITLE OF TEST
-	     String title = "page title for this test!";
-	     int marginTop = 15;
+	     String title = "Page title for this test!";
+	     int marginTop = 10;
 	     PDPageContentStream stream = new PDPageContentStream(doc, page,true,true);
 	     stream.setFont( font, 16 );
 	     int fontSize = 16; // Or whatever font size you want.
 	     float titleWidth = font.getStringWidth(title) / 1000 * fontSize;
 	     float titleHeight = font.getFontDescriptor().getFontBoundingBox().getHeight() / 1000 * fontSize;
-	     
-	     float var1 = (page.getMediaBox().getWidth() - titleWidth) / 2;
-	     float var2 = page.getMediaBox().getHeight() - marginTop - titleHeight;
-//	     System.out.println("comp = "+ var1 + " and " + var2);
+	    
 	     stream.beginText();
 	     stream.moveTextPositionByAmount((page.getMediaBox().getWidth() - titleWidth) / 2, page.getMediaBox().getHeight() - marginTop - titleHeight);
 	     stream.drawString(title);
 	     stream.endText();
 	     stream.close();
-	     
-	     
-	     //create multple lines from single line 
-	     String testline = "You can convert it to a BufferedImage by copying its raster to a new BufferedImage. Serach for 'convert image to bufferedimage";
-	     List<String> linestrings = WriteLine(testline);
-	     for(String word : linestrings){
-	    	 System.out.println(word);
-	     }
-	     
-
-	    //CREATE THE PDF MAIN LOOP
-		for(int i = 0; i < numofquestions; i++){
-			String qrCodeText = "Question "+i;
-	        String filePath = "/Users/angellopozo/Dropbox/My Code/java/MainRabbitMongo/Resources/Quest"+i+".png";
-	        
-	        String fileType = "png";
-	        File qrFile = new File(filePath);
-	        BufferedImage aQRImage = null;
-	        aQRImage = createQRImage(qrFile, qrCodeText, sizeofQRCode, fileType);
-	        System.out.println("DONE with "+i);
-	        
-	        PDXObjectImage QRImage = new PDJpeg(doc, aQRImage);
-	        PDXObjectImage bubble = new PDJpeg(doc, bubblgeImage);
-	        PDPageContentStream contentStream = new PDPageContentStream(doc, page,true,true);
-	        
-	        contentStream.drawImage( QRImage, 30, (620-120*i) );
-	        contentStream.setFont( font, 12 );
-
-	        WriteQuestionToPDF(contentStream, i, QA,bubble);
-	        
-	        
-	        contentStream.close();
-		}//end of for loop
-	
-		//contentStream.close();
-		doc.save( "/Users/angellopozo/Dropbox/My Code/java/MainRabbitMongo/Resources/CreatedPDF_TEST.pdf");
-		doc.close();
 		
+	}//end of writetitletopdf
 	
-	}//end of main
+	
+	
+	
 	
 	
 	
@@ -181,18 +243,26 @@ public class CreatePDF_TEST {
 		int linebuff = 0;
 	     for(String textline : linestrings){
 //	    	 System.out.println(line);
-	 		mycontentStream.beginText();
-	 		mycontentStream.moveTextPositionByAmount(150 , (715-(120*i-linebuff)));        
-	 		mycontentStream.drawString(textline);
-	 		mycontentStream.endText();	    	 
-	 		linebuff = linebuff - 15 ;
+	    	 if(linestrings.size() > 1){
+		 		mycontentStream.beginText();
+		 		mycontentStream.moveTextPositionByAmount(105 , ((735)-(105*i-linebuff))); //position of the question text 
+		 		mycontentStream.drawString(textline);
+		 		mycontentStream.endText();
+	 			linebuff = linebuff - 15 ;
+	 		  }//end of if
+	    	 else{
+			 		mycontentStream.beginText();
+			 		mycontentStream.moveTextPositionByAmount(105 , ((725-linebuff)-(105*i-linebuff))); //position of the question text 
+			 		mycontentStream.drawString(textline);
+			 		mycontentStream.endText();
+	    	 }
 	     }//end of linestring for
 	     
 		int shiftint = 0;
 		for(String s: QA[i].PossibleAnswers){ 
-			mycontentStream.drawImage(bubble, 150, (680-20*shiftint - 120*i));
+			mycontentStream.drawImage(bubble, 100, (700-18*shiftint - 105*i)); //position of the bubble
 			mycontentStream.beginText();
-			mycontentStream.moveTextPositionByAmount(170 , (685-20*shiftint - 120*i) );        
+			mycontentStream.moveTextPositionByAmount(125 , (705-18*shiftint - 105*i) );//position of the possible answer text        //var*shiftint : var i the distance between questions
 			mycontentStream.drawString(ReturnQuestionLetter(shiftint) + " " + QA[i].PossibleAnswers[shiftint] );
 			mycontentStream.endText();
 			shiftint = shiftint + 1;
@@ -203,32 +273,12 @@ public class CreatePDF_TEST {
 	
 	
 	
-//	private static void WriteQuestionToPDF(PDPageContentStream mycontentStream, int i, Question[] QA, PDXObjectImage bubble) throws IOException{
-//		mycontentStream.beginText();
-//		mycontentStream.moveTextPositionByAmount(150 , (715-120*i) );        
-//		mycontentStream.drawString(QA[i].Question );
-//		mycontentStream.endText();
-//		System.out.println("size of question " + QA[i].Question.length());
-//		 
-//		int shiftint = 0;
-//		for(String s: QA[i].PossibleAnswers){ 
-//			mycontentStream.drawImage(bubble, 150, (680-20*shiftint - 120*i));
-//			mycontentStream.beginText();
-//			mycontentStream.moveTextPositionByAmount(170 , (685-20*shiftint - 120*i) );        
-//			mycontentStream.drawString(ReturnQuestionLetter(shiftint) + " " + QA[i].PossibleAnswers[shiftint] );
-//			mycontentStream.endText();
-//			shiftint = shiftint + 1;
-//		}
-//		
-//	}//end of Write Question to PDF
-	
-	
 	
 	private static List<String> WriteLine(String Text){		
 		//check for how long a line 
 		String[] olines = new String[3];
 		List<String> output = new ArrayList();
-		System.out.println("size of question " + Text.length());
+//		System.out.println("size of question " + Text.length());
 		if(Text.length() > 74){//74 is the maximum character length to the wall for a line
 			String[] words = Text.split(" ");
 			String ostring = "";
@@ -269,13 +319,13 @@ public class CreatePDF_TEST {
 		
 		String OutString = null;
 		switch (myselect){
-			case 0: OutString = "A) ";
+			case 0: OutString = "A)";
 					break;
-			case 1: OutString = "B) ";
+			case 1: OutString = "B)";
 					break;
-			case 2: OutString = "C) ";
+			case 2: OutString = "C)";
 					break;		
-			case 3: OutString = "D) ";
+			case 3: OutString = "D)";
 					break;
 			default: OutString = "ERROR DO NOT USE THIS PAPER!!!! ";
 					break;
@@ -285,7 +335,37 @@ public class CreatePDF_TEST {
 	
 	
 	
-	private static BufferedImage createQRImage(File qrFile, String qrCodeText, int size, String fileType) throws WriterException, IOException { //size = sizeofQRCode
+//	private static BufferedImage createQRImage(File qrFile, String qrCodeText, int size, String fileType) throws WriterException, IOException {
+//	        // Create the ByteMatrix for the QR-Code that encodes the given String
+//	        Hashtable hintMap = new Hashtable();
+//	        hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+//	        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+//	        BitMatrix byteMatrix = qrCodeWriter.encode(qrCodeText,
+//	                BarcodeFormat.QR_CODE, size, size, hintMap);
+//	        // Make the BufferedImage that are to hold the QRCode
+//	        int matrixWidth = byteMatrix.getWidth();
+//	        BufferedImage image = new BufferedImage(matrixWidth, matrixWidth, BufferedImage.TYPE_INT_RGB);
+//	        image.createGraphics();
+//	 
+//	        Graphics2D graphics = (Graphics2D) image.getGraphics();
+//	        graphics.setColor(Color.WHITE);
+//	        graphics.fillRect(0, 0, matrixWidth, matrixWidth);
+//	        // Paint and save the image using the ByteMatrix
+//	        graphics.setColor(Color.BLACK);
+//	 
+//	        for (int i = 0; i < matrixWidth; i++) {
+//	            for (int j = 0; j < matrixWidth; j++) {
+//	                if (byteMatrix.get(i, j)) {
+//	                    graphics.fillRect(i, j, 1, 1);
+//	                }
+//	            }
+//	        }
+//	        ImageIO.write(image, fileType, qrFile);
+//	        return image;
+//	    }//end of CreateQRImage
+	
+	
+	private static BufferedImage createQRImage(String qrCodeText, int size, String fileType) throws WriterException, IOException {
         // Create the ByteMatrix for the QR-Code that encodes the given String
         Hashtable hintMap = new Hashtable();
         hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
@@ -294,18 +374,14 @@ public class CreatePDF_TEST {
                 BarcodeFormat.QR_CODE, size, size, hintMap);
         // Make the BufferedImage that are to hold the QRCode
         int matrixWidth = byteMatrix.getWidth();
-        BufferedImage image = new BufferedImage(matrixWidth, matrixWidth,
-                BufferedImage.TYPE_INT_RGB);
+        BufferedImage image = new BufferedImage(matrixWidth, matrixWidth, BufferedImage.TYPE_INT_RGB);
         image.createGraphics();
  
         Graphics2D graphics = (Graphics2D) image.getGraphics();
-//        graphics.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-        
         graphics.setColor(Color.WHITE);
         graphics.fillRect(0, 0, matrixWidth, matrixWidth);
         // Paint and save the image using the ByteMatrix
         graphics.setColor(Color.BLACK);
-//        graphics.scale(.8, .8);
  
         for (int i = 0; i < matrixWidth; i++) {
             for (int j = 0; j < matrixWidth; j++) {
@@ -317,6 +393,7 @@ public class CreatePDF_TEST {
 //        ImageIO.write(image, fileType, qrFile);
         return image;
     }//end of CreateQRImage
+
 	
 	
 	
